cf634ac930312e2e556afc46e721339d47cbc130
Volodymyr Dziubak <Volodymyr.Dzyubak@gmail.com>
2025-03-21 21:28:20 -0400

    prettier tailwlind organize imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className="w-screen h-screen grid place-content-center">
+    <div className="grid h-screen w-screen place-content-center">
       <div className="border border-amber-600 p-10">I am here</div>
     </div>
   );
