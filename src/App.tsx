58faaa0df47fe283491d2e658baba43a919bc737
Volodymyr Dziubak <Volodymyr.Dzyubak@gmail.com>
2025-03-21 21:36:35 -0400

    shadcn ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
     <div className="grid h-screen w-screen place-content-center">
-      <div className="border border-amber-600 p-10">I am here</div>
+      <div className="border border-amber-600 p-10">
+        <div>I am here</div>
+
+        <Button>I am a button!</Button>
+      </div>
     </div>
   );
 }
