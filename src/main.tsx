49a2e8a6c4b9a9902f7ce806a9b23e85b2e29726
Volodymyr Dziubak <Volodymyr.Dzyubak@gmail.com>
2025-03-24 12:18:12 -0400

    vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
@@ -8,6 +9,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
       <App />
+      <Analytics />
     </ThemeProvider>
   </StrictMode>,
 );
