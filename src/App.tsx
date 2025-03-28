15af02929e7a901fff383046a10e5bb54e0e07d1
Volodymyr Dziubak <Volodymyr.Dzyubak@gmail.com>
2025-03-27 23:00:05 -0400

    cpp deductions

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import Decimal from "decimal.js";
 import { useEffect, useState } from "react";
 import { ModeToggle } from "./components/mode-toggle";
-import { Card, CardContent, CardHeader, CardTitle } from "./components/ui/card";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "./components/ui/accordion";
+import { Card, CardContent } from "./components/ui/card";
 import { Input } from "./components/ui/input";
 import { Label } from "./components/ui/label";
 import {
@@ -11,6 +17,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from "./components/ui/select";
+import { cn } from "./lib/utils";
 
 const federalRates = [0.15, 0.205, 0.26, 0.29, 0.33];
 const federalBrackets = [57375, 57375, 63132, 75532];
@@ -23,6 +30,10 @@ const MbBrackets = [47564, 53636];
 
 const SKRates = [0.105, 0.125, 0.145];
 const SKBrackets = [53463, 99287];
+
+const cppExempt = 3500;
+const cppBrackets = [67800, 9900];
+const cppRates = [0.0595, 0.04];
 
 const provinceCodeMap = {
   NF: "Newfoundland and Labrador",
@@ -63,6 +74,24 @@ function calculateTaxes(
   }
 
   return taxPerBracket;
+}
+
+function calculateCpp(income: number): number[] {
+  const cpp: number[] = [];
+
+  let num = income - cppExempt;
+
+  for (const [index, bracket] of cppBrackets.entries()) {
+    if (num > 0) {
+      cpp.push(Math.min(bracket, num) * cppRates[index]);
+    } else {
+      cpp.push(0);
+    }
+
+    num = num - bracket;
+  }
+
+  return cpp;
 }
 
 function sumUpTo(brackets: number[], index: number) {
@@ -125,215 +154,120 @@ function getProvinceRates(province: keyof typeof provinceCodeMap) {
 }
 
 function TaxesTable(props: {
-  title: string;
   taxes: number[];
   brackets: number[];
   rates: number[];
+  className: string;
 }) {
   return (
-    <Card className="flex-1">
-      <CardHeader>
-        <CardTitle>{props.title}</CardTitle>
-      </CardHeader>
-
-      <CardContent className="text-muted-foreground grid grid-cols-[auto_1fr_auto] items-center gap-x-4 gap-y-2 text-sm">
-        {props.rates.map((rate, index) => (
-          <>
-            <div>{getBracketLabel(props.brackets, index)}</div>
-            <div>@ {(rate * 100).toFixed(2)}%</div>
-            <div className="text-right">
-              {props.taxes[index]?.toLocaleString("en-CA", {
-                style: "currency",
-                currency: "CAD",
-              }) ?? 0}
-            </div>
-          </>
-        ))}
-
-        <div className="border-border text-foreground col-span-full grid grid-cols-[1fr_auto] gap-2 border-t pt-2 text-base">
-          <div className="">Total</div>
+    <div
+      className={cn("grid grid-cols-[auto_1fr_auto] gap-4", props.className)}
+    >
+      {props.rates.map((rate, index) => (
+        <div key={index} className="col-span-full grid grid-cols-subgrid">
+          <div>{getBracketLabel(props.brackets, index)}</div>
+          <div>@ {(rate * 100).toFixed(2)}%</div>
           <div className="text-right">
-            {props.taxes
-              .reduce((a, b) => a + b, 0)
-              .toLocaleString("en-CA", {
-                style: "currency",
-                currency: "CAD",
-              })}
+            {props.taxes[index]?.toLocaleString("en-CA", {
+              style: "currency",
+              currency: "CAD",
+            }) ?? 0}
           </div>
         </div>
-      </CardContent>
-    </Card>
+      ))}
+    </div>
   );
 }
 
-function SummaryAnnual(props: {
-  federalTaxes: number[];
-  provincialTaxes: number[];
-  income: string;
-}) {
-  let incomeNumeric;
-  try {
-    incomeNumeric = new Decimal(props.income).toNumber();
-  } catch {
-    incomeNumeric = 0;
-  }
-
-  const federalTaxes = props.federalTaxes.reduce((a, b) => a + b, 0);
-  const provincialTaxes = props.provincialTaxes.reduce((a, b) => a + b, 0);
-
+function CppTable(props: { cpp: number[]; className?: string }) {
   return (
-    <Card className="flex-1">
-      <CardHeader>
-        <CardTitle>Annual Summary</CardTitle>
-      </CardHeader>
+    <div
+      className={cn("grid grid-cols-[auto_1fr_auto] gap-4", props.className)}
+    >
+      <div>
+        $0.00 -{" "}
+        {cppExempt.toLocaleString("en-CA", {
+          style: "currency",
+          currency: "CAD",
+        })}
+      </div>
+      <div></div>
+      <div className="text-right">Exempt</div>
 
-      <CardContent className="text-muted-foreground grid grid-cols-[1fr_auto] items-center gap-x-4 gap-y-2 text-sm">
-        <div>Net income</div>
-        <div className="text-right">
-          {incomeNumeric.toLocaleString("en-CA", {
-            style: "currency",
-            currency: "CAD",
-          })}
-        </div>
+      <div>
+        {cppExempt.toLocaleString("en-CA", {
+          style: "currency",
+          currency: "CAD",
+        })}{" "}
+        -{" "}
+        {(cppBrackets[0] + cppExempt).toLocaleString("en-CA", {
+          style: "currency",
+          currency: "CAD",
+        })}
+      </div>
+      <div>@ {(cppRates[0] * 100).toFixed(2)}%</div>
+      <div className="text-right">
+        {props.cpp[0]?.toLocaleString("en-CA", {
+          style: "currency",
+          currency: "CAD",
+        }) ?? 0}
+      </div>
 
-        <div>Federal taxes</div>
-        <div className="text-right">
-          {federalTaxes.toLocaleString("en-CA", {
-            style: "currency",
-            currency: "CAD",
-          })}
-        </div>
-
-        <div>Provincial taxes</div>
-        <div className="text-right">
-          {provincialTaxes.toLocaleString("en-CA", {
-            style: "currency",
-            currency: "CAD",
-          })}
-        </div>
-
-        <div className="border-border text-foreground col-span-full grid grid-cols-[1fr_auto] gap-2 border-t pt-2 text-base">
-          <div className="">Post-tax income</div>
-          <div className="text-right">
-            {(incomeNumeric - federalTaxes - provincialTaxes).toLocaleString(
-              "en-CA",
-              {
-                style: "currency",
-                currency: "CAD",
-              },
-            )}
-          </div>
-        </div>
-      </CardContent>
-    </Card>
-  );
-}
-
-function SummaryMonthly(props: {
-  federalTaxes: number[];
-  provincialTaxes: number[];
-  income: string;
-}) {
-  let incomeNumeric;
-  try {
-    incomeNumeric = new Decimal(props.income).div(12).toNumber();
-  } catch {
-    incomeNumeric = 0;
-  }
-
-  const federalTaxes = props.federalTaxes.reduce((a, b) => a + b, 0) / 12;
-  const provincialTaxes = props.provincialTaxes.reduce((a, b) => a + b, 0) / 12;
-
-  return (
-    <Card className="flex-1">
-      <CardHeader>
-        <CardTitle>Monthly Summary</CardTitle>
-      </CardHeader>
-
-      <CardContent className="text-muted-foreground grid grid-cols-[1fr_auto] items-center gap-x-4 gap-y-2 text-sm">
-        <div>Net income</div>
-        <div className="text-right">
-          {incomeNumeric.toLocaleString("en-CA", {
-            style: "currency",
-            currency: "CAD",
-          })}
-        </div>
-
-        <div>Federal taxes</div>
-        <div className="text-right">
-          {federalTaxes.toLocaleString("en-CA", {
-            style: "currency",
-            currency: "CAD",
-          })}
-        </div>
-
-        <div>Provincial taxes</div>
-        <div className="text-right">
-          {provincialTaxes.toLocaleString("en-CA", {
-            style: "currency",
-            currency: "CAD",
-          })}
-        </div>
-
-        <div className="border-border text-foreground col-span-full grid grid-cols-[1fr_auto] gap-2 border-t pt-2 text-base">
-          <div className="">Post-tax income</div>
-          <div className="text-right">
-            {(incomeNumeric - federalTaxes - provincialTaxes).toLocaleString(
-              "en-CA",
-              {
-                style: "currency",
-                currency: "CAD",
-              },
-            )}
-          </div>
-        </div>
-      </CardContent>
-    </Card>
+      <div>
+        {(cppBrackets[0] + cppExempt).toLocaleString("en-CA", {
+          style: "currency",
+          currency: "CAD",
+        })}{" "}
+        -{" "}
+        {(cppBrackets[0] + cppBrackets[1] + cppExempt).toLocaleString("en-CA", {
+          style: "currency",
+          currency: "CAD",
+        })}
+      </div>
+      <div>@ {(cppRates[1] * 100).toFixed(2)}%</div>
+      <div className="text-right">
+        {props.cpp[1]?.toLocaleString("en-CA", {
+          style: "currency",
+          currency: "CAD",
+        }) ?? 0}
+      </div>
+    </div>
   );
 }
 
 function App() {
-  const [income, setIncome] = useState<string>("");
-  const [epb, setEpb] = useState<string>("");
+  const [incomeValue, setIncomeValue] = useState<string>("100000");
   const [fed, setFed] = useState<number[]>([]);
   const [prov, setProv] = useState<number[]>([]);
+  const [income, setIncome] = useState<number>(0);
+  const [cpp, setCpp] = useState<number[]>([]);
   const [province, setProvince] = useState<keyof typeof provinceCodeMap>("ON");
 
   useEffect(() => {
     let num;
     try {
-      num = new Decimal(income).toNumber();
+      num = new Decimal(incomeValue).toNumber();
     } catch {
       num = 0;
     }
 
-    let epbNum;
-    try {
-      epbNum = new Decimal(epb).mul(12).toNumber();
-    } catch {
-      epbNum = 0;
-    }
+    setIncome(num);
 
-    const taxPerBracket = calculateTaxes(
-      num + epbNum,
-      federalBrackets,
-      federalRates,
-    );
+    setFed(calculateTaxes(num, federalBrackets, federalRates));
+    setCpp(calculateCpp(num));
 
     switch (province) {
       case "ON":
-        setProv(calculateTaxes(num + epbNum, ONBrackets, ONRates));
+        setProv(calculateTaxes(num, ONBrackets, ONRates));
         break;
       case "MB":
-        setProv(calculateTaxes(num + epbNum, MbBrackets, MbRates));
+        setProv(calculateTaxes(num, MbBrackets, MbRates));
         break;
       case "SK":
-        setProv(calculateTaxes(num + epbNum, SKBrackets, SKRates));
+        setProv(calculateTaxes(num, SKBrackets, SKRates));
         break;
     }
-
-    setFed(taxPerBracket);
-  }, [epb, income, province]);
+  }, [incomeValue, province]);
 
   return (
     <div className="h-screen w-screen">
@@ -345,75 +279,161 @@ function App() {
         <ModeToggle />
       </div>
 
-      <div className="mx-auto flex max-w-[300px] flex-col gap-10 px-4 py-10">
-        <div className="flex flex-col gap-4">
-          <Select
-            value={province}
-            onValueChange={(value) =>
-              setProvince(value as keyof typeof provinceCodeMap)
-            }
-          >
-            <SelectTrigger className="w-full">
-              <SelectValue placeholder="Select a province" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem value="ON">Ontario</SelectItem>
-              <SelectItem value="MB">Manitoba</SelectItem>
-              <SelectItem value="SK">Saskatchewan</SelectItem>
-            </SelectContent>
-          </Select>
+      <div className="mx-auto flex max-w-[800px] justify-center gap-10 px-4 py-10">
+        <div className="flex basis-1/3 flex-col gap-4">
+          <div className="flex flex-col gap-1">
+            <Label htmlFor="province">Province</Label>
+            <Select
+              value={province}
+              onValueChange={(value) =>
+                setProvince(value as keyof typeof provinceCodeMap)
+              }
+            >
+              <SelectTrigger className="w-full" id="province">
+                <SelectValue placeholder="Select a province" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="ON">Ontario</SelectItem>
+                <SelectItem value="MB">Manitoba</SelectItem>
+                <SelectItem value="SK">Saskatchewan</SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
 
           <div className="flex flex-col gap-1">
-            <Label htmlFor="income">Net Annual Income</Label>
+            <Label htmlFor="income">Gross Annual Income</Label>
             <Input
-              value={income}
+              value={incomeValue}
               type="number"
               className="shrink-0"
               id="income"
-              onChange={(e) => setIncome(e.target.value)}
-            />
-          </div>
-
-          <div className="flex flex-col gap-1">
-            <Label htmlFor="epb">Taxable benefits (monthly)</Label>
-            <Input
-              value={epb}
-              type="number"
-              className="shrink-0"
-              id="epb"
-              onChange={(e) => setEpb(e.target.value)}
+              onChange={(e) => setIncomeValue(e.target.value)}
             />
           </div>
         </div>
-      </div>
 
-      <div className="mx-auto mb-6 flex max-w-[1000px] gap-4 px-4">
-        <TaxesTable
-          title="Federal Taxes"
-          taxes={fed}
-          brackets={federalBrackets}
-          rates={federalRates}
-        />
+        <div className="flex basis-2/3 flex-col gap-4">
+          <Card className="py-2">
+            <CardContent>
+              <div className="border-border flex items-center border-b py-4 pr-8 font-medium">
+                <div>Gross annual income</div>
 
-        <TaxesTable
-          title={`Provincial Taxes (${provinceCodeMap[province]})`}
-          taxes={prov}
-          brackets={getProvinceBrackets(province)}
-          rates={getProvinceRates(province)}
-        />
-      </div>
+                <div className="flex-1 text-right">
+                  {income.toLocaleString("en-CA", {
+                    style: "currency",
+                    currency: "CAD",
+                  })}
+                </div>
+              </div>
 
-      <div className="mx-auto flex max-w-[800px] gap-4 px-4">
-        <SummaryAnnual
-          federalTaxes={fed}
-          provincialTaxes={prov}
-          income={income}
-        />
-        <SummaryMonthly
-          federalTaxes={fed}
-          provincialTaxes={prov}
-          income={income}
-        />
+              <Accordion type="multiple" className="w-full">
+                <AccordionItem value="item-1">
+                  <AccordionTrigger className="text-base">
+                    <div>Federal taxes</div>
+
+                    <div className="flex-1 text-right">
+                      {fed
+                        .reduce((acc, curr) => acc - curr, 0)
+                        .toLocaleString("en-CA", {
+                          style: "currency",
+                          currency: "CAD",
+                        })}
+                    </div>
+                  </AccordionTrigger>
+
+                  <AccordionContent>
+                    <TaxesTable
+                      taxes={fed}
+                      brackets={federalBrackets}
+                      rates={federalRates}
+                      className="text-muted-foreground pr-8 text-sm"
+                    />
+                  </AccordionContent>
+                </AccordionItem>
+
+                <AccordionItem value="item-2">
+                  <AccordionTrigger className="text-base">
+                    <div>Provincial taxes ({provinceCodeMap[province]})</div>
+
+                    <div className="flex-1 text-right">
+                      {prov
+                        .reduce((acc, curr) => acc - curr, 0)
+                        .toLocaleString("en-CA", {
+                          style: "currency",
+                          currency: "CAD",
+                        })}
+                    </div>
+                  </AccordionTrigger>
+
+                  <AccordionContent>
+                    <TaxesTable
+                      taxes={prov}
+                      brackets={getProvinceBrackets(province)}
+                      rates={getProvinceRates(province)}
+                      className="text-muted-foreground pr-8 text-sm"
+                    />
+                  </AccordionContent>
+                </AccordionItem>
+
+                <AccordionItem value="item-3">
+                  <AccordionTrigger className="text-base">
+                    <div>Canada Pension Plan (CPP)</div>
+
+                    <div className="flex-1 text-right">
+                      {cpp
+                        .reduce((acc, curr) => acc - curr, 0)
+                        .toLocaleString("en-CA", {
+                          style: "currency",
+                          currency: "CAD",
+                        })}
+                    </div>
+                  </AccordionTrigger>
+
+                  <AccordionContent>
+                    <CppTable
+                      cpp={cpp}
+                      className="text-muted-foreground pr-8 text-sm"
+                    />
+                  </AccordionContent>
+                </AccordionItem>
+              </Accordion>
+
+              <div className="border-border flex items-center border-t py-4 pr-8 font-medium">
+                <div>Net annual income</div>
+
+                <div className="flex-1 text-right">
+                  {(
+                    income -
+                    fed.reduce((acc, curr) => acc + curr, 0) -
+                    prov.reduce((acc, curr) => acc + curr, 0)
+                  )
+                    // - cpp
+                    .toLocaleString("en-CA", {
+                      style: "currency",
+                      currency: "CAD",
+                    })}
+                </div>
+              </div>
+
+              {/* <div className="text-muted-foreground flex items-center pr-8 pb-4 text-sm font-medium">
+                  <div>Net monthly income</div>
+
+                  <div className="flex-1 text-right">
+                    {(
+                      income - 
+                          fed.reduce((acc, curr) => acc + curr, 0) -
+                            prov.reduce((acc, curr) => acc + curr, 0) - 
+                              cpp
+                        )
+                    ).toLocaleString("en-CA", {
+                      style: "currency",
+                      currency: "CAD",
+                    })}
+                  </div>
+                </div> */}
+            </CardContent>
+          </Card>
+        </div>
       </div>
     </div>
   );
